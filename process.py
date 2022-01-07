63a44ecb055eae1f446cacfa3cf8fd9e38732c4c
Stetson Done <scdone@gmail.com>
2022-01-07 11:50:25 -0700

    some comments added

--- process.py
+++ process.py
@@ -4,11 +4,17 @@ log_file = open("um-server-01.txt")
 
 
 # def sales_reports(log_file):
+#line above defines a function
 #     for line in log_file:
+#line above initiates a for loop in the file
 #         line = line.rstrip()
+#line above removes whitespace
 #         day = line[0:3]
+#line above defines day as the first 3 chars.
 #         if day == "Tue":
+#line above says "if the day = Tue..."
 #             print(line)
+#line above prints the line if it meets the if statement for beginning with "tue":
 
 #The function above is going through the txt file we opened. It defines where day is in the data. If the "day" is "Tue" (for tuesday), it then prints that line of data.
 # 
